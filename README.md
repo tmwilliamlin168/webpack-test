dc087d67cdbaf8154560d48ab7f0845c6223e3ef
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2022-05-23 09:33:54 +0800

    boilerplate

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.freecodecamp.org/news/how-to-create-a-react-app-with-a-node-backend-the-complete-guide/
---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
