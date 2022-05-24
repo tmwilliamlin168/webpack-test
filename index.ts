215df6bf87d8b845751d066a2ffdd6ffea0fd832
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2022-05-24 08:25:18 +0800

    webpack to compile component

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import webpack from 'webpack';
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,3 +11,40 @@ app.listen(PORT, () => {
 });
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Stuff to consider:
+// webpack mode, BABEL_ENV development vs production
+// concurrent compilations is bad?
+// 'module' output is experimental? https://github.com/webpack/webpack/issues/2933
+// too lazy to think abt browserslist
+const path1 = path.resolve(__dirname, 'user_components');
+process.env.BABEL_ENV = 'production';
+webpack({
+  mode: 'production',
+  devtool: 'source-map',
+  target: ['web', 'es5'],
+  entry: path.resolve(path1, 'input.jsx'),
+  output: { path: path1, filename: 'output.js', library: {type: 'module'} },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: path1,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  experiments: { outputModule: true },
+}, (err, stats) => {
+  if (err || stats?.hasErrors()) {
+    console.error(err || stats?.compilation.errors);
+  }
+  console.log('Finished compiling?');
+});
+
+app.get('/component.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'user_components/output.js'));
+});
